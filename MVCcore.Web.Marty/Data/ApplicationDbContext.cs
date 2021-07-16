ef1cd6808493e8a495ad9e72206ad423219bd4f9
martycode49 <marty49@cegetel.net>
2021-07-17 00:10:35 +0200

    Add migration

---
+++ MVCcore.Web.Marty/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCcore.Web.Marty.Models;
+
+namespace MVCcore.Web.Marty.Data
+{
+    public class ApplicationDbContext : DbContext
+    {   
+        // Dependence Injection : create a constructor 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
